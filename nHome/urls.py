ff8086bcbd5a7cec3e5827739e36a4b798573064
Md.Rakib <mdrakibk849@gmail.com>
2023-10-30 02:21:11 +0600

    Scrol Bir add or Catagory button add

--- nHome/urls.py
+++ nHome/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import homepage
+from .views import homepage, post,catagory
 
 urlpatterns = [
     path('', homepage),
+    path('nHome/<slug:url>', post),
+    path('catagory/<slug:url>', catagory)
+
 
 ]
--- nHome/views.py
+++ nHome/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from nHome.models import Post
+from nHome.models import Post, Category
 
 
 # Create your views here.
 def homepage(request):
     # load all the post form db (10)
     post = Post.objects.all()[:11]
+
+    cats = Category.objects.all()
     data = {
-        'posts': post
+        'posts': post,
+        'cats': cats
     }
 
     return render(request, 'home.html', data)
+
+
+def post(request, url):
+    post = Post.objects.get(url=url)
+    cats = Category.objects.all()
+
+    return render(request, 'posts.html', {'post': post, 'cats': cats})
+
+
+def catagory(request, url):
+    cat = Category.objects.get(url=url)
+    posts = Post.objects.filter(cat=cat)
+    return render(request, 'catagory.html', {'cat':cat, 'posts':posts})
